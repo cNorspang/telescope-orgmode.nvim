31200be5d03590c3c2fcf494f8ad14942803acc7
Casper Norspang <cbn@targit.com>
2026-08-04 06:44:27 +0200

    wip: mini support

--- lua/telescope-orgmode/mini/headings.lua
+++ lua/telescope-orgmode/mini/headings.lua
@@ -19,7 +19,7 @@ local function show(buf_id, items_arr, query)
 
   for i, x in ipairs(items_arr) do
     local file_stub = vim.fs.basename(x.filename)
-    local line_nr = x.headline.position.start_line
+    local line_nr = x.headline.line_number
     local tags_string = table.concat(x.headline.tags, ":")
     local level_string = string.rep('*', x.headline.level)
 
