66097b26a81984cf5841f2676d7c426f66417673
Casper Norspang <cbn@targit.com>
2026-08-06 05:28:58 +0200

    wip: mini support

--- lua/telescope-orgmode/mini/headings.lua
+++ lua/telescope-orgmode/mini/headings.lua
@@ -109,6 +109,7 @@ local function custom_choose()
     pick.get_picker_state().windows.target,
     function() operations.navigate_to(current) end
   )
+  return true
 end
 
 function build_picker(items, resolved_opts)
